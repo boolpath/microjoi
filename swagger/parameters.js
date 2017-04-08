af7696567aabc296f00cecfe265b6b76e0be9772
jorgezaccaro <jorgezaccaro@gmail.com>
2017-04-08 17:38:17 +0800

    fix(swagger): missing fields and support for references

--- swagger/parameters.js
+++ swagger/parameters.js
@@ -4,14 +4,21 @@ const joi2swagger = require('joi-to-swagger')
 
 function parse(object, location, parameters = []) {
   for (let property in object) {
-    if (!object[property].isJoi) {
-      parse(object[property], property, parameters)
+    let schema = object[property]
+    if (!schema.isJoi) {
+      parse(schema, property, parameters)
     } else {
-      let {swagger, definitions} = joi2swagger(object[property])
+      let {swagger} = joi2swagger(schema)
       let parameter = Object.assign({
         name: property,
-        in: location
+        in: location || property,
+        description: schema._description || undefined,
+        required: schema._flags.presence === 'required' || undefined
       }, swagger)
+      if (parameter['$ref']) {
+        parameter.schema = {'$ref': swagger['$ref']}
+        delete parameter['$ref']
+      }
       parameters.push(parameter)
     }
   }
