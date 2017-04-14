cabce9bd022324e2b0573dd0e9fe9e1189c5097d
jorgezaccaro <jorgezaccaro@gmail.com>
2017-04-14 19:39:58 +0800

    feat(swagger): added support for /definitions directory

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ const def = definitionsPath || api
 
 const path2schemas = path.resolve(cwd, api, 'schemas')
 const path2swagger = path.resolve(cwd, api, 'swagger.json')
-const path2defines = path.resolve(cwd, def, 'definitions.js')
+const path2defines = path.resolve(cwd, def, 'definitions')
 const schemas = requireDirectory(module, path2schemas)
 
 let definitions = {}
 let parameters = {}
 
 try {parameters  = require(path2swagger)} catch (error) {/*not found*/}
-try {definitions = require(path2defines)} catch (error) {/*not found*/}
+try {definitions = require(path2defines)} catch (error) {
+  try {definitions = requireDirectory(module, path2defines)}
+  catch (error) {/*not found*/}
+}
 
 const swagger = generateSwagger(schemas, definitions, parameters)
 const swaggerFile = JSON.stringify(swagger, null, 3)
